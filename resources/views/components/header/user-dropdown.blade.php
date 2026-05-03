8b063b69a9773f0c33b21183afa81ac777a20712
GilangAqshal <gilangais16@gmail.com>
2026-05-03 21:38:04 +0700

    CRUD Paket Member & Paket Harian

--- resources/views/components/header/user-dropdown.blade.php
+++ resources/views/components/header/user-dropdown.blade.php
@@ -116,21 +116,24 @@
         </ul>
 
         <!-- Sign Out -->
-        {{-- <form method="POST" action="#">
-            @csrf --}}
-            <a
-                href="/auth/login"
-                class="flex items-center w-full gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
-                @click="closeDropdown()"
-            >
-                <span class="text-gray-500 group-hover:text-gray-700 dark:group-hover:text-gray-300">
-                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
-                    </svg>
-                </span>
-                Sign out
-            </a>
-        {{-- </form> --}}
+<!-- Form Logout Tersembunyi -->
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    @csrf
+</form>
+
+<!-- Tombol Sign Out (Link yang memicu Form) -->
+<a
+    href="#"
+    onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+    class="flex items-center w-full gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
+>
+    <span class="text-gray-500 group-hover:text-gray-700 dark:group-hover:text-gray-300">
+        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
+        </svg>
+    </span>
+    Sign out
+</a>
     </div>
     <!-- Dropdown End -->
 </div>
